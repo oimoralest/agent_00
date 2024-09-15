ca5ccf6f7b7966aca60a82b8fd196530fe7cac02
joseAlfaro25 <jale1325@gmail.com>
2024-09-15 11:10:59 -0500

    new connection node

--- frontend/src/route/agent.tsx
+++ frontend/src/route/agent.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -7,88 +7,96 @@ import ReactFlow, {
   applyEdgeChanges,
   applyNodeChanges,
   Panel,
-} from 'reactflow';
+} from "reactflow";
 import { useSelector } from "react-redux";
-import 'reactflow/dist/style.css';
-import { Button, Card, CardBody, useDisclosure } from "@nextui-org/react";
+import "reactflow/dist/style.css";
+import { Button, Card, CardBody } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 
 import Editable from "../component/editable/main";
 import NodePanel from "../component/nodePanel";
-import { Link } from "react-router-dom";
 
+const CustomEdge = ({ id, sourceX, sourceY, targetX, targetY, markerEnd }) => {
+  const edgePath = `M${sourceX},${sourceY} L${targetX},${targetY}`;
+  return (
+    <g>
+      <path
+        id={id}
+        style={{
+          stroke: "#000",
+          strokeWidth: 2,
+          strokeDasharray: "5,5",
+          animation: "dash 1.5s linear infinite",
+        }}
+        d={edgePath}
+        markerEnd={markerEnd}
+      />
+      <style>
+        {`
+          @keyframes dash {
+            to {
+              stroke-dashoffset: -20;
+            }
+          }
+        `}
+      </style>
+    </g>
+  );
+};
 
+// Build Nodes
 const buildNodes = (nodes) => {
-  console.log("Building nodes");
-  console.log(nodes);
   const labelMapper = {
-    "timer": (node) => {
+    timer: (node) => {
       return `Timer ${node?.data?.time}s`;
     },
-    "llm": (node) => {
+    llm: (node) => {
       console.log(node);
       return `LLM ${node?.name}`;
     },
-    "input": (node) => {
+    input: (node) => {
       return `Input ${node?.name}`;
     },
-    "prompt": (node) => {
+    prompt: (node) => {
       return `Prompt ${node?.name}`;
-    }
-
-  }
-  return nodes.map((node, index) => {
-    return {
-      id: node.id,
-      position: { x: 200, y: 100 * (index + 1) },
-      data: { label: labelMapper[node.type](node) }
-    }
-  });
-}
-
+    },
+  };
+  return nodes?.map((node, index) => ({
+    id: node.id,
+    position: node.position || { x: 200 * (index + 1), y: 100 * (index + 1) },
+    data: { label: node.name || node.type },
+    style: { border: "1px solid #777", padding: "10px", borderRadius: "8px" }, // Node style
+  }));
+};
 
 const Agent = () => {
   const agent = useSelector((state) => state.agent);
 
-  // const initialNodes = [
-  //   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  //   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-  // ];
-  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
   const [openEditable, setOpenEditable] = useState(false);
   const [nodeType, setNodeType] = useState("");
   const [nodes, setNodes] = useState([]);
-  const [edges, setEdges] = useState(initialEdges);
+  const [edges, setEdges] = useState([]);
 
-  // TODO: Built nodes and edges with the agent data
   useEffect(() => {
-    if (!agent.nodes) return;
-    setNodes(buildNodes(agent.nodes));
-    console.log(nodes);
-  }, [agent?.nodes]);
+    setNodes(buildNodes(nodes));
+    setEdges(edges);
+  }, []);
 
   // Handlers
-  // Actions on nodes
-  // TODO: When selecting a node, open the editable panel
-  const onNodesChange = useCallback((changes) => {
-    console.log("Nodes changes");
-    console.log(changes);
-    setNodes((nodes) => applyNodeChanges(changes, nodes));
-  }, [setEdges]);
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
 
-  // Actions on edges
-  const onEdgesChange = useCallback((changes) => {
-    console.log("Edges changes");
-    console.log(changes);
-    setEdges((edges) => applyEdgeChanges(changes, edges));
-  }, [setEdges]);
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
 
-  // Actions on connecting nodes
-  const onConnect = useCallback((params) => {
-    console.log("Connect");
-    console.log(params);
-    setEdges((eds) => addEdge(params, eds));
-  }, [setEdges]);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    []
+  );
 
   return (
     <div className="flex flex-wrap w-screen h-screen flex-col gap-2 justify-center items-center">
@@ -98,14 +106,28 @@ const Agent = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        fitView
+        edgeTypes={{ default: CustomEdge }} // Usar el CustomEdge
       >
         <Panel position="top-left">
-          <Button
-            isIconOnly={true}
-          >
-            <Link to={`/project/${agent.project_id}`}>
-              <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
+          <Button isIconOnly={true}>
+            <Link to={`/project/${agent?.project_id}`}>
+              <svg
+                className="w-6 h-6 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h14M5 12l4-4m-4 4 4 4"
+                />
               </svg>
             </Link>
           </Button>
@@ -113,20 +135,27 @@ const Agent = () => {
         <Panel position="top-center">
           <Card shadow="sm">
             <CardBody>
-              <h1>{agent.name}</h1>
+              <h1>{agent?.name || ""}</h1>
             </CardBody>
           </Card>
         </Panel>
         <Panel position="bottom-center">
-          <NodePanel setOpenEditable={setOpenEditable} setNodeType={setNodeType} />
+          <NodePanel
+            setOpenEditable={setOpenEditable}
+            setNodeType={setNodeType}
+          />
         </Panel>
-        <Editable isOpen={openEditable} type={nodeType} onOpenChange={setOpenEditable} />
+        <Editable
+          isOpen={openEditable}
+          type={nodeType}
+          onOpenChange={setOpenEditable}
+        />
         <Controls />
         <MiniMap />
         <Background variant="dots" gap={12} size={1} />
       </ReactFlow>
     </div>
-  )
-}
+  );
+};
 
 export default Agent;
